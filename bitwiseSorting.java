af64e54053b80b1e735bc54324d064e6504cff00
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-08 23:20:11 +0530

    Leetcode - Bit Manipulation - Sort Integers by The Number of 1 Bits

--- bitwiseSorting.java
+++ bitwiseSorting.java
@@ -1,7 +1,6 @@
 /*You are given an integer array arr.
  Sort the integers in the array in ascending order by the number of 1's in their binary representation 
 and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
-
 Return the array after sorting it.
 
 Example 1:
