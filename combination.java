44b45d36a53b3a4502692582e6b219b2afef5b06
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-03-24 19:44:54 +0530

    sdf

--- combination.java
+++ combination.java
@@ -1,8 +1,38 @@
-import pyautogui
-import time
+/*Given an array of distinct integers candidates and a target integer target, return a list 
+of all unique combinations of candidates where the chosen numbers sum to target. You may 
+return the combinations in any order.
 
-code_to_type = """
-import java.util.*;
+The same number may be chosen from candidates an unlimited number of times. Two 
+combinations are unique if the 
+frequency
+ of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to 
+target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: 4
+2 3 6 7
+ 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+Input: 3
+2 3 5 
+8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+Input: 1
+2
+1
+Output: []
+*/import java.util.*;
 class combination{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
@@ -37,15 +67,3 @@ class combination{
         combination(A, i + 1, n, sum, target, op, ans);
     }
 }
-
-
-"""
-
-
-time.sleep(10)
-def avoid_auto_complete():
-    pyautogui.hotkey('esc', 'space', 'backspace', 'enter', 'space', 'ctrl', 'left')
-lines = list(code_to_type.split("\n"))
-for line in lines:
-        pyautogui.typewrite(line, interval=0)
-        avoid_auto_complete()
