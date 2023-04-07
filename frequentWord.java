c3536e9db98cb617cb12131004d7ff32e68859c3
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-04-07 15:01:42 +0530

    asdf

--- frequentWord.java
+++ frequentWord.java
@@ -39,30 +39,35 @@ Sample Output-2:
 ----------------
 [are, egg, case]
 */
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        String arr[]=sc.next().split(",");
-        int n=sc.nextInt();
-        System.out.println(cal(arr,n));
+        String words[]=sc.next().split(",");
+        int k=sc.nextInt();
+        System.out.println(cal(words,k));
     }
-    public static List<String> cal(String arr[],int n){
-        Map<String,Integer> m=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(!m.containsKey(arr[i])){
-                m.put(arr[i],1);
-            }else{
-                m.put(arr[i],m.get(arr[i])+1);
-            }
-        }
-        List<String> ls=new ArrayList<>();
-        ls.addAll(m.keySet());
-        Collections.sort(ls,(a,b)-> m.get(a).equals(m.get(b)) ? a.compareTo(b) : m.get(b)-m.get(a));
-        List<String> mainls=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            mainls.add(ls.get(i));
-        }
-        return mainls;
+    public static List<String> cal(String words[],int k){
+        public List<String> topKFrequent(String[] words, int k) {
+            Map<String,Integer> m=new HashMap<>();
+             for(int i=0;i<words.length;i++){
+                 if(!m.containsKey(words[i])){
+                     m.put(words[i],1);
+                 }else{
+                     m.put(words[i],m.get(words[i])+1);
+                 }
+             }
+             List<String> ls=new ArrayList<>();
+             ls.addAll(m.keySet());
+             Collections.sort(ls,(a,b)-> m.get(a).equals(m.get(b)) ? a.compareTo(b) : m.get(b)-m.get(a));
+             for(int i=ls.size()-1;i>=k;i--){
+                 ls.remove(ls.get(i));
+             }
+             return ls;
+         }
     }
 }
